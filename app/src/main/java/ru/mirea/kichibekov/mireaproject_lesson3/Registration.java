5d04ce5ab47edb58e04e52dca8b8ee4f829d7b60
RakhmanKichibekov <kichibekovrahman@yandex.ru>
2023-10-05 21:43:06 +0300

    Проверка на установленый AnyDesk

--- app/src/main/java/ru/mirea/kichibekov/mireaproject_lesson3/Registration.java
+++ app/src/main/java/ru/mirea/kichibekov/mireaproject_lesson3/Registration.java
@@ -1,11 +1,20 @@
 package ru.mirea.kichibekov.mireaproject_lesson3;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,6 +26,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import ru.mirea.kichibekov.mireaproject_lesson3.databinding.ActivityRegistrationBinding;
@@ -27,6 +38,7 @@ public class Registration extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ActivityRegistrationBinding binding;
     private FirebaseAuth mAuth;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +48,11 @@ public class Registration extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         @SuppressLint("HardwareIds")
         String m_androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-        System.out.println("Индивидуальный код "+m_androidId);
-        binding.textID.setText(m_androidId);
+        //Доп задания
+        searchApps();
+        System.out.println("Индивидуальный код " + m_androidId);
+        binding.textID.setText("ID вашего устройства=" + m_androidId);
+        //Конец доп заданий
         binding.signedInButtons.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +71,61 @@ public class Registration extends AppCompatActivity {
         });
 // [END initialize_auth]
     }
+
+    @SuppressLint("SetTextI18n")
+    private void searchApps(){
+        PackageManager pm = getPackageManager();
+        @SuppressLint("QueryPermissionsNeeded") List<ApplicationInfo> apps = pm.getInstalledApplications(0);
+        List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
+        View externalLayout = LayoutInflater.from(this).inflate(R.layout.banner_layout, null, false);
+        TextView textBanner = externalLayout.findViewById(R.id.bannerTextView);
+        for(ApplicationInfo app : apps) {
+            //checks for flags; if flagged, check if updated system app
+            if((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+                installedApps.add(app);
+                //it's a system app, not interested
+            } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                //Discard this one
+                //in this case, it should be a user-installed app
+            } else {
+                installedApps.add(app);
+            }
+        }
+
+        // Создаем список для хранения имен установленных приложений
+        List<String> appNames = new ArrayList<>();
+
+        // Перебираем приложения и получаем их имена
+        for (ApplicationInfo appInfo : installedApps) {
+            String appName = appInfo.loadLabel(pm).toString();
+            appNames.add(appName);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                appNames);
+        for (String app: appNames) {
+            if (app.equals("AnyDesk")){
+                textBanner.setText("У вас установлено запрещённое приложение AnyDesk, войти не получится");
+                showBlockingBanner();
+            }
+        }
+        binding.textApp.setAdapter(adapter);
+    }
+    private void showBlockingBanner() {
+        // Получаем корневой Layout вашей активности
+        RelativeLayout rootLayout = findViewById(R.id.rootLayout);
+
+        // Создаем баннер из разметки
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View bannerView = inflater.inflate(R.layout.banner_layout, rootLayout, false);
+        // Добавляем баннер в корневой Layout
+        rootLayout.addView(bannerView);
+
+        Dialog dialog = new Dialog(this, android.R.style.Theme_Translucent_NoTitleBar);
+        dialog.setContentView(R.layout.banner_layout);
+        dialog.setCancelable(false); // Запретить закрытие диалога при нажатии вне его области
+        dialog.show();
+    }
+
     // [START on_start_check_user]
     @Override
     public void onStart() {
@@ -64,6 +134,7 @@ public class Registration extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
+
     // [END on_start_check_user]
     private void updateUI(FirebaseUser user) {
         if (user != null) {
